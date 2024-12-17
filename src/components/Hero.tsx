500b29200431c2e0979c9b1685811cc20a5506d4
najafsikander <najafsikander@yahoo.com>
2024-12-17 12:05:13 +0400

    Completed a disgusting hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return (
+        <>
+            <section className="flex justify-center items-center w-full h-[88vh] bg-slate-600 bg-hero-background bg-cover">
+                <div className="w-[60%] h-[30%] bg-white/10 rounded-md flex justify-center items-center text-white">
+                    <div className="block text-center">
+                    <h1 className="relative text-5xl text-white">Welcome and witness my shenanigans!</h1>
+                    <h3 className="text-xl">Here I will demonstrate some magical concepts and libraries. Prepare yourself to be amazed</h3>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-background': "url('/images/react-bg.webp')"
+      }
+    },
   },
   plugins: [],
 }
