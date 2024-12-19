8eaea9d22911f107225e4c49fe0aa4c94014b926
najafsikander <najafsikander@yahoo.com>
2024-12-19 16:57:21 +0400

    Fixed form card shadow

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -3,7 +3,7 @@ const FormPage = () => {
     return (
         <>
             <section className="w-full bg-white flex justify-center">
-                <div className="w-[80%] h-[50vh] text-center mt-20 border-2 text-2xl rounded-md border-grey shadow-xl px-4 py-4 shadow-md">
+                <div className="w-[80%] h-[50vh] text-center mt-20 border-2 text-2xl rounded-md border-grey shadow-xl px-4 py-4">
                    <Form/> 
                 </div>
             </section>
